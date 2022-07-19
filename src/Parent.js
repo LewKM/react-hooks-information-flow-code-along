05c50b0ebd8ad395151959c5a39afca93c0bd03c
Lewis Mwendwa <lewis.mwendwa@student.moringaschool.com>
2022-07-19 07:34:41 +0300

    Done

--- src/Parent.js
+++ src/Parent.js
@@ -4,12 +4,25 @@ import Child from "./Child";
 
 function Parent() {
   const randomColor = getRandomColor();
-  const [color, setColor] = useState(randomColor);
+  const [popColor, setColor] = useState(randomColor);
+  const [childrenColor, setChildrenColor] = useState("#FFF");
+
+  function handleChangeColor(newChildColor) {
+    const newRandomColor = getRandomColor();
+    setColor(newRandomColor);
+    setChildrenColor(newChildColor);
+  } 
+
+  // function handleClick(newChildColor){
+  //   let newRandomColor = getRandomColor()
+  //   setColor(newRandomColor)
+  //   setChildrenColor(newChildColor)
+  // }
 
   return (
-    <div className="parent" style={{ backgroundColor: color }}>
-      <Child />
-      <Child />
+    <div className="parent" style={{ backgroundColor: popColor }}>
+      <Child onChangeColor={handleChangeColor} color={childrenColor} />
+      <Child onChangeColor={handleChangeColor} color={childrenColor} />
     </div>
   );
 }
